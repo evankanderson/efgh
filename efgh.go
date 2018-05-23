9079301d9dbb7b759e533a8ef254ce09d3f9541c
Evan Anderson <evan.k.anderson@gmail.com>
2018-05-23 14:41:41 -0700

    Refactor interfaces and files
    * Move context into a struct from independent accessors
    * Move handler & invoker to a separate file from the main interface
    * Restructure to prepare for support for structured content mode HTTP transport (NYI)

--- efgh.go
+++ efgh.go
@@ -3,24 +3,11 @@ package efgh
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"reflect"
 	"time"
 )
-
-// key is an unexported type for keys defined in this package.
-// This prevents collisions with keys defined in other packages.
-type key int
-
-// headerKey is the key for http.Header values in Contexts. clients
-// use specific accessors like `EventTime` instead of using this key
-// directly.
-var headerKey key
 
 func Start(function interface{}) {
 	handler, err := wrap(function)
@@ -33,142 +20,35 @@ func Start(function interface{}) {
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
-func wrap(function interface{}) (http.Handler, error) {
-	h := FunctionHandler{
-		f: reflect.ValueOf(function),
-	}
-
-	// Used to extract types
-	ctxType := reflect.TypeOf((*context.Context)(nil)).Elem()
-	errType := reflect.TypeOf((*error)(nil)).Elem()
-	t := reflect.TypeOf(function)
-	if t.Kind() != reflect.Func {
-		return nil, fmt.Errorf("%r is not a function", t)
-	}
-
-	if t.NumIn() > 2 {
-		return nil, fmt.Errorf("%r takes too many arguments", t)
-	}
-	if t.NumIn() > 0 {
-		if t.In(0).Implements(ctxType) {
-			h.needsContext = true
-		} else {
-			h.inType = t.In(0)
-		}
-	}
-	if t.NumIn() == 2 {
-		if !h.needsContext {
-			return nil, fmt.Errorf("First argument must be of type context.Context: %r", t)
-		}
-		h.inType = t.In(1)
-	}
-
-	if t.NumOut() > 2 {
-		return nil, fmt.Errorf("%r returns too many outputs", t)
-	}
-	if t.NumOut() > 0 {
-		if t.Out(0).Implements(errType) {
-			h.hasError = true
-		} else {
-			h.outType = t.Out(0)
-		}
-	}
-	if t.NumOut() == 2 {
-		if h.hasError || !t.Out(1).Implements(errType) {
-			return nil, fmt.Errorf("Must return (data, error) with two arguments: %r", t)
-		}
-		h.hasError = true
-	}
-
-	return h, nil
+type CloudEventContext struct {
+	// Type of occurence which has happened.
+	EventType string `json:"eventType"`
+	// The version of the `eventType`; this is producer-specific.
+	EventTypeVersion string `json:"eventTypeVersion,omitempty"`
+	// The version of the CloudEVents specification used by the event.
+	CloudEventsVersion string `json:"cloudEventsVersion"`
+	// The event producer; this is a URI, but exact syntax is producer-specific.
+	Source string `json:"source"`
+	// ID of the event; must be non-empty and unique within the scope of the producer.
+	EventID string `json:"eventID"`
+	// Timestamp of when the event happened.
+	EventTime time.Time `json:"eventTime,omitempty"`
+	// A link to the schema that the `data` attribute adheres to.
+	SchemaURL string `json:"schemaURL,omitempty"`
+	// Describes the data encoding format.
+	ContentType string `json:"contentType,omitempty"`
+	// Additional metadata without a well-defined structure.
+	Extensions map[string]json.RawMessage `json:"extensions,omitempty"`
 }
 
-func (fh FunctionHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		rw.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	ctx := req.Context()
-	ctx = context.WithValue(ctx, headerKey, req.Header)
-	in, err := ioutil.ReadAll(req.Body)
-	//	var out []byte
-	if err != nil {
-		rw.WriteHeader(http.StatusExpectationFailed)
-		return
-	}
-	// Handle different function types.
-	out, err := fh.Invoke(ctx, in)
-	if err != nil {
-		rw.WriteHeader(http.StatusInternalServerError)
-		io.WriteString(rw, err.Error())
-		return
-	}
-	rw.WriteHeader(http.StatusOK)
-	rw.Write(out)
+func CloudEvent(ctx context.Context) (CloudEventContext, bool) {
+	r, ok := ctx.Value(contextKey).(CloudEventContext)
+	return r, ok
 }
 
-func (fh FunctionHandler) Invoke(ctx context.Context, in []byte) (out []byte, err error) {
-	var args []reflect.Value
-	if fh.needsContext {
-		args = append(args, reflect.ValueOf(ctx))
-	}
-	if fh.inType != nil {
-		data := reflect.New(fh.inType)
-		if fh.inType.Kind() == reflect.Struct {
-			log.Printf("Decoding '%s' to struct\n", in)
-			if err = json.Unmarshal(in, data.Interface()); err != nil {
-				log.Printf("Unable to unmarshall: %r\n", err)
-				return
-			}
-		} else if fh.inType.Kind() == reflect.Array && fh.inType.Elem().Kind() == reflect.Uint8 {
-			data.SetBytes(in)
-		}
+// key is an unexported type for keys defined in this package.
+// This prevents collisions with keys defined in other packages.
+type key int
 
-		args = append(args, data.Elem())
-		log.Printf("Data is: %s (%s/%s)\n", data.String(), fh.inType.Kind(), data.Interface())
-	}
-	log.Printf("Calling with args: %r\n", args)
-	response := fh.f.Call(args)
-	i := 0
-	if fh.outType != nil {
-		// TODO: unpack type
-		log.Printf("Should unpack %r", response[i])
-		i++
-	}
-	if fh.hasError {
-		var ok bool
-		if err, ok = response[i].Interface().(error); ok {
-		} else {
-			log.Printf("Type of err(%d) is %s\n", i, response[i].Type())
-			//			err = fmt.Errorf("Could not extract error from %r", response)
-		}
-	}
-
-	return
-}
-
-func EventTime(ctx context.Context) (time.Time, error) {
-	ts := getHeader(ctx, "Ce-Eventtime")
-	return time.Parse(time.RFC3339, ts)
-}
-
-func EventId(ctx context.Context) string {
-	return getHeader(ctx, "Ce-Eventid")
-}
-
-func getHeader(ctx context.Context, key string) string {
-	h, ok := ctx.Value(headerKey).(http.Header)
-	if !ok {
-		return ""
-	}
-	return h.Get(key)
-}
-
-type FunctionHandler struct {
-	// The function in question
-	f reflect.Value
-
-	// metadata about the signature
-	needsContext, hasError bool
-	inType, outType        reflect.Type
-}
+// contextKey is the key for CloudEventContext values in Contexts.
+var contextKey key
---
+++ handler.go
@@ -0,0 +1,175 @@
+package efgh
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+	"time"
+)
+
+// Implementation of HTTP processing for an event function.
+
+type FunctionHandler struct {
+	// The function in question
+	f reflect.Value
+
+	// metadata about the signature
+	needsContext, hasError bool
+	inType, outType        reflect.Type
+}
+
+func (fh FunctionHandler) Invoke(ctx context.Context, in []byte) (out []byte, err error) {
+	var args []reflect.Value
+	if fh.needsContext {
+		args = append(args, reflect.ValueOf(ctx))
+	}
+	if fh.inType != nil {
+		data := reflect.New(fh.inType)
+		if fh.inType.Kind() == reflect.Struct {
+			if err = json.Unmarshal(in, data.Interface()); err != nil {
+				log.Printf("Unable to unmarshall as JSON: %r\n", err)
+				return nil, err
+			}
+		} else if fh.inType.Kind() == reflect.Array && fh.inType.Elem().Kind() == reflect.Uint8 {
+			data.SetBytes(in)
+		}
+		args = append(args, data.Elem())
+	}
+
+	response := fh.f.Call(args)
+	if fh.outType != nil {
+		// TODO: unpack type
+		log.Printf("Should unpack %r", response[0])
+	}
+	if fh.hasError {
+		var ok bool
+		if err, ok = response[len(response)-1].Interface().(error); ok {
+		}
+	}
+
+	return
+}
+
+func (fh FunctionHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	var in []byte
+	var ctx context.Context
+	var err error
+
+	if strings.HasPrefix(req.Header.Get("Content-Type"), "application/cloudevents") {
+		in, ctx, err = convertStructured(req)
+	} else {
+		in, ctx, err = convertBinary(req)
+	}
+	if err != nil {
+		rw.Header().Set("Content-Type", "text/plain")
+		rw.WriteHeader(http.StatusExpectationFailed)
+		io.WriteString(rw, err.Error())
+		return
+	}
+	// Handle different function types.
+	out, err := fh.Invoke(ctx, in)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(rw, err.Error())
+		return
+	}
+	// TODO: extract headers and/or handle binary vs structured on response.
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(out)
+}
+
+// Handle Structed Content Mode (https://github.com/cloudevents/spec/blob/v0.1/http-transport-binding.md#32-structured-content-mode)
+func convertStructured(req *http.Request) ([]byte, context.Context, error) {
+	return nil, nil, errors.New("Structured output not supported yet.")
+}
+
+// Handle Binary Content Mode (https://github.com/cloudevents/spec/blob/v0.1/http-transport-binding.md#31-binary-content-mode)
+func convertBinary(req *http.Request) ([]byte, context.Context, error) {
+	cex := CloudEventContext{
+		EventType:          req.Header.Get("CE-EventType"),
+		EventTypeVersion:   req.Header.Get("CE-EventTypeVersion"),
+		CloudEventsVersion: req.Header.Get("CE-CloudEventsVersion"),
+		Source:             req.Header.Get("CE-Source"),
+		EventID:            req.Header.Get("CE-EventID"),
+		SchemaURL:          req.Header.Get("CE-SchemaURL"),
+		ContentType:        req.Header.Get("Content-Type"),
+	}
+	ts := req.Header.Get("CE-EventTime")
+	var err error
+	if ts != "" {
+		cex.EventTime, err = time.Parse(time.RFC3339, req.Header.Get("CE-EventTime"))
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	// TODO: handle extensions
+
+	ctx := context.WithValue(req.Context(), contextKey, cex)
+	in, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	return in, ctx, nil
+}
+
+// Convert a function to an HTTP Handler
+func wrap(function interface{}) (http.Handler, error) {
+	h := FunctionHandler{
+		f: reflect.ValueOf(function),
+	}
+
+	// Used to extract types
+	ctxType := reflect.TypeOf((*context.Context)(nil)).Elem()
+	errType := reflect.TypeOf((*error)(nil)).Elem()
+	t := reflect.TypeOf(function)
+	if t.Kind() != reflect.Func {
+		return nil, fmt.Errorf("%r is not a function", t)
+	}
+
+	if t.NumIn() > 2 {
+		return nil, fmt.Errorf("%r takes too many arguments", t)
+	}
+	if t.NumIn() > 0 {
+		if t.In(0).Implements(ctxType) {
+			h.needsContext = true
+		} else {
+			h.inType = t.In(0)
+		}
+	}
+	if t.NumIn() == 2 {
+		if !h.needsContext {
+			return nil, fmt.Errorf("First argument must be of type context.Context: %r", t)
+		}
+		h.inType = t.In(1)
+	}
+
+	if t.NumOut() > 2 {
+		return nil, fmt.Errorf("%r returns too many outputs", t)
+	}
+	if t.NumOut() > 0 {
+		if t.Out(0).Implements(errType) {
+			h.hasError = true
+		} else {
+			h.outType = t.Out(0)
+		}
+	}
+	if t.NumOut() == 2 {
+		if h.hasError || !t.Out(1).Implements(errType) {
+			return nil, fmt.Errorf("Must return (data, error) with two arguments: %r", t)
+		}
+		h.hasError = true
+	}
+
+	return h, nil
+}
